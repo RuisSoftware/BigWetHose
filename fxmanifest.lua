247eb7576dc025a8184244ea97851662dd15f8e7
Tobey Luciando <42970574+symeRobinson@users.noreply.github.com>
2020-08-31 12:26:12 -0500

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Syme Robinson (Robinson#7901)'
+description 'Do /hose to enable a water stream from the Fire Extinguisher'
+version '1.0.0'
+
+client_script 'client.lua'
+server_script 'server.lua'
